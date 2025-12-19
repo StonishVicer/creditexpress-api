e683e6625be59154f8d477718927fde2374bddf4
colmen4z <saircolmenarez2006@gmail.com>
2025-12-19 13:54:42 -0400

    Se creo la API Resource para customers

---
+++ app/Http/Resources/CustomersResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomersResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nombre' => $this->name,
+            'cedula' => $this->number_id,
+            'telefono' => $this->phone,
+            'direccion' => $this->address,
+            'clasificacion_de_pago' => $this->payment_classification,
+            'estado' => $this->status,
+        ];
+    }
+}
