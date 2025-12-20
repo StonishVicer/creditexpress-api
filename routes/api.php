a09c4eb20c36c33e275645c95f61d63048c32004
colmen4z <saircolmenarez2006@gmail.com>
2025-12-20 18:51:38 -0400

    Se agregaron las rutas API para loans

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\CustomersController;
+use App\Http\Controllers\Api\LoansController;
 
 Route::get('/customers', [CustomersController::class, 'index']);
 Route::get('/customers/{id}', [CustomersController::class, 'show']);
@@ -11,3 +12,9 @@ Route::post('/customers', [CustomersController::class, 'store']);
 Route::delete('/customers/{id}', [CustomersController::class, 'destroy']);
 Route::put('/customers/{id}', [CustomersController::class, 'update']);
 Route::patch('/customers/{id}', [CustomersController::class, 'updatePartial']);
+
+Route::get('/loans', [LoansController::class, 'index']);
+Route::get('/loans/{id}', [LoansController::class, 'show']);
+Route::post('/loans', [LoansController::class, 'store']);
+Route::delete('/loans/{id}', [LoansController::class, 'destroy']);
+Route::put('/loans/{id}', [LoansController::class, 'update']);
