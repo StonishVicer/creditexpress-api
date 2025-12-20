662e8154e469ee8a79a4e07f3ef4916e9331e523
colmen4z <saircolmenarez2006@gmail.com>
2025-12-20 18:56:16 -0400

    Migracion de loans

---
+++ database/migrations/2025_12_19_220110_create_loans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->float('principal_amount', 10, 2);
+            $table->smallInteger('interest_rate');
+            $table->smallInteger('payment_term');
+            $table->smallInteger('number_installments');
+            $table->float('interest_to_collect', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
