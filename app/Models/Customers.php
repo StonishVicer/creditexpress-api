3eb49d76cd7358185ce38f9224ef8a931ac58cc1
colmen4z <saircolmenarez2006@gmail.com>
2025-12-20 19:36:23 -0400

    Se modifico el modelo customers para que funcionara con la relacion a loans

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Customers extends Model
 {
@@ -19,4 +20,9 @@ class Customers extends Model
         'payment_classification',
         'status',
     ];
+
+    public function loan(): BelongsTo
+    {
+        return $this->belongsTo(Loans::class);
+    }
 }
