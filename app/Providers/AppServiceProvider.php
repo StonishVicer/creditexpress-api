01f51938c590f7239f3a46cac7123b440ad32381
colmen4z <saircolmenarez2006@gmail.com>
2025-12-20 18:52:05 -0400

    Se bindeo el repository y la interface de loans

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,14 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+
 use App\Repositories\Contracts\CustomersRepositoryInterface;
 use App\Repositories\Eloquent\CustomersRepository;
+
+use App\Repositories\Contracts\LoansRepositoryInterface;
+use App\Repositories\Eloquent\LoansRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->app->bind(CustomersRepositoryInterface::class, CustomersRepository::class);
+        $this->app->bind(LoansRepositoryInterface::class, LoansRepository::class);
     }
 
     public function boot(): void
