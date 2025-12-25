ea44bb929a22c1f6b1b982490387e4d2b925e524
colmen4z <saircolmenarez2006@gmail.com>
2025-12-25 17:27:38 -0400

    Se modifico el servicio para que se comunicara con el repository pattern de loans_status

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,12 +10,16 @@ use App\Repositories\Eloquent\CustomersRepository;
 use App\Repositories\Contracts\LoansRepositoryInterface;
 use App\Repositories\Eloquent\LoansRepository;
 
+use App\Repositories\Contracts\LoansStatusRepositoryInterface;
+use App\Repositories\Eloquent\LoansStatusRepository;
+
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->app->bind(CustomersRepositoryInterface::class, CustomersRepository::class);
         $this->app->bind(LoansRepositoryInterface::class, LoansRepository::class);
+        $this->app->bind(LoansStatusRepositoryInterface::class, LoansStatusRepository::class);
     }
 
     public function boot(): void
